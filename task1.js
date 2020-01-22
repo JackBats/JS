0af27d829baf1fdddda3c92ab37e16d5dd49eb7c
Yevhen <jbat98@gmail.com>
2020-01-22 14:57:03 +0100

    first commit

---
+++ task1.js
@@ -0,0 +1,21 @@
+let money = prompt("Ваш бюджет на месяц?"),
+    time = prompt("Введите дату в формате");
+
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+};
+
+let ask1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    ask2 = prompt("Во сколько обойдется?"),
+    ask3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    ask4 = prompt("Во сколько обойдется?");
+
+appData.expenses[ask1] = ask2;
+appData.expenses[ask3] = ask4;
+
+alert(appData.budget / 30);
